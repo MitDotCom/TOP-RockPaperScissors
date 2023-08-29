55595829d26a115bb1b827c5f9917cbff5d2fe3e
Mitchell Comrie <Mitchellrcomrie@gmail.com>
2023-08-28 17:51:23 -0700

    Write pseudocode algorithm for script.js

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//Pseudocode//
+
+// Create an array 'RPS' containing strings: "rock","paper","scissors"
+//
+// Define a function play() containing getComputerChoice() function and get playerChoice() functions
+// Define function getComputerChoice()  that creates a variable 'computerChoice' and assigns it a random choice string from 'RPS'
+// Call getComputerChoice()
+// Define a function getPlayerChoice() that creates a variable playerChoice with initial input value from prompt function reading: "Enter either: 'rock', 'paper', or 'scissors'"
+//   -if value is not equal to either 'rock' or 'paper' or 'scissors', re-prompt user for correct input value
+//
+// if computerChoice is equal to playerChoice
+//   -print "It's a tie"
+//   -break
+// else if playerChoice is equal to 'rock' and computerChoice is equal to 'paper'
+//   -print "Computer wins"
+//   -break
+// else if playerChoice is equal to 'rock' and computerChoice is equal to 'scissors'
+//   -print "Player wins"
+//   -break
+// else if playerChoice is equal to 'paper' and computerChoice is equal to 'rock'
+//   -print "Player wins"
+//   -break
+// else if playerChoice is equal to 'paper' and computerChoice is equal to 'scissors'
+//   -print "Computer wins"
+//   -break
+// else if playerChoice is equal to 'scissors' and computerChoice is equal to 'rock'
+//   -print "Computer wins"
+//   -break
+// else if playerChoice is equal to 'scissors' and computerChoice is equal to 'paper'
+//   -print "Player wins"
+//   -break
+//
+// Define a function playAgain() that creates a variable 'again' with an initial input value from prompt function reading: "Do you want to play again? Enter: 'y' or 'n'"
+//   -while again is not equal to either 'y' or 'no'
+//     -prompt user with same text prompt and update 'again' variable with the users input
+//   -if 'again' is equal to 'y'
+//     -call play() function
+//   else
+//     -print "Thanks for playing"
