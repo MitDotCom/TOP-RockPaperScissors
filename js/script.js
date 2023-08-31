f301e0117936947464e3b51369cbbd25146c139b
Mitchell Comrie <Mitchellrcomrie@gmail.com>
2023-08-31 16:54:44 -0700

    Write conditional statements for calcWinner() function

--- js/script.js
+++ js/script.js
@@ -39,24 +39,47 @@ function calcWinner();
 //   -break
     break;
 // else if playerChoice is equal to 'rock' and computerChoice is equal to 'paper'
-//   -print "Computer wins"
+    else if (playerChoice === 'rock') && (computerChoice === 'paper') {
+//   -print "Computer wins"        
+        console.log("Computer wins");
+    }
 //   -break
+        break;
 // else if playerChoice is equal to 'rock' and computerChoice is equal to 'scissors'
-//   -print "Player wins"
+    else if (playerChoice === 'rock') && (computerChoice === 'scissors') {
+    //   -print "Player wins"
+        console.log("Player wins");
+        }
 //   -break
+        break;
 // else if playerChoice is equal to 'paper' and computerChoice is equal to 'rock'
-//   -print "Player wins"
+    else if (playerChoice === 'paper') && (computerChoice === 'rock') {
+    //   -print "Player wins"
+        console.log("Player wins");
+        }
 //   -break
+        break;
 // else if playerChoice is equal to 'paper' and computerChoice is equal to 'scissors'
+    else if (playerChoice === 'paper') && (computerChoice === 'scissors') {
 //   -print "Computer wins"
+        console.log("Computer wins");
+        }
 //   -break
+        break;
 // else if playerChoice is equal to 'scissors' and computerChoice is equal to 'rock'
+    else if (playerChoice === 'scissors') && (computerChoice === 'rock') {
 //   -print "Computer wins"
+        console.log("Computer wins");
+        }
 //   -break
+        break;
 // else if playerChoice is equal to 'scissors' and computerChoice is equal to 'paper'
+    else if (playerChoice === 'scissors') && (computerChoice === 'paper') {
 //   -print "Player wins"
+        console.log("Player wins");
+        }
 //   -break
-//
+        break;
 // Define a function playAgain() that creates a variable 'again' with an initial input value from prompt function reading: "Do you want to play again? Enter: 'y' or 'n'"
 //   -while again is not equal to either 'y' or 'no'
 //     -prompt user with same text prompt and update 'again' variable with the users input
