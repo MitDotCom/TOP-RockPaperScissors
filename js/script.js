87bd1b8654fd653984becb772f04e6388871afdc
Mitchell <Mitchellrcomrie@gmail.com>
2023-10-17 19:42:26 -0700

    Refactor script

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
+const playAgain = document.querySelector(".playAgain");
 
-const RPS = [rock, paper, scissors];
-
+let RPS = [rock, paper, scissors];
 let rounds = 1;
-
 let playerChoice;
 let computerWins = 0;
 let playerWins = 0;
 
-rock.addEventListener("click", function () {
-  playerChoice = rock.textContent;
-  playRound(playerChoice);
-});
-
-paper.addEventListener("click", function () {
-  playerChoice = paper.textContent;
-  playRound(playerChoice);
-});
-
-scissors.addEventListener("click", function () {
-  playerChoice = scissors.textContent;
-  playRound(playerChoice);
-});
-
 function getComputerChoice() {
   let computerChoice = RPS[Math.floor(Math.random() * RPS.length)];
   return computerChoice;
+}
+
+function playRound(playerChoice) {
+  computerChoice = getComputerChoice();
+}
+
+function main() {
+  rock.addEventListener("click", function () {
+    playerChoice = rock.textContent;
+    playRound(playerChoice);
+  });
+
+  paper.addEventListener("click", function () {
+    playerChoice = paper.textContent;
+    playRound(playerChoice);
+  });
+
+  scissors.addEventListener("click", function () {
+    playerChoice = scissors.textContent;
+    playRound(playerChoice);
+  });
 }
 
 function calcWinner(computerChoice, playerChoice) {
@@ -36,61 +41,48 @@ function calcWinner(computerChoice, playerChoice) {
     console.log(`It's a tie. You both chose ${playerChoice}.`);
     rounds++;
     console.log(`Round: ${rounds}`);
-    console.log(".....................");
   } else if (playerChoice === "rock" && computerChoice === "paper") {
     console.log("Computer wins. Paper beats rock.");
     rounds++;
     computerWins++;
-    console.log("Computer wins. Paper beats rock.");
     console.log(`Computer wins: ${computerWins}`);
     console.log(`Player wins: ${playerWins}`);
     console.log(`Round: ${rounds}`);
-    console.log(".....................");
   } else if (playerChoice === "rock" && computerChoice === "scissors") {
     console.log("Player wins. Rock beats scissors.");
     rounds++;
     playerWins++;
-    console.log("Player wins. Rock beats scissors.");
     console.log(`Computer wins: ${computerWins}`);
     console.log(`Player wins: ${playerWins}`);
     console.log(`Round: ${rounds}`);
-    console.log(".....................");
   } else if (playerChoice === "paper" && computerChoice === "rock") {
     console.log("Player wins. Paper beats rock.");
     rounds++;
     playerWins++;
-    console.log("Player wins. Paper beats rock.");
     console.log(`Computer wins: ${computerWins}`);
     console.log(`Player wins: ${playerWins}`);
     console.log(`Round: ${rounds}`);
-    console.log(".....................");
   } else if (playerChoice === "paper" && computerChoice === "scissors") {
     console.log("Computer wins. Scissors beats paper.");
     rounds++;
     computerWins++;
-    console.log("Computer wins. Scissors beats paper.");
     console.log(`Computer wins: ${computerWins}`);
     console.log(`Player wins: ${playerWins}`);
     console.log(`Round: ${rounds}`);
-    console.log(".....................");
   } else if (playerChoice === "scissors" && computerChoice === "rock") {
     console.log("Computer wins. Rock beats scissors.");
     rounds++;
     computerWins++;
-    console.log("Computer wins. Rock beats scissors.");
     console.log(`Computer wins: ${computerWins}`);
     console.log(`Player wins: ${playerWins}`);
     console.log(`Round: ${rounds}`);
-    console.log(".....................");
   } else if (playerChoice === "scissors" && computerChoice === "paper") {
     console.log("Player wins. Scissors beats paper.");
     rounds++;
     playerWins++;
-    console.log("Player wins. Scissors beats paper.");
     console.log(`Computer wins: ${computerWins}`);
     console.log(`Player wins: ${playerWins}`);
     console.log(`Round: ${rounds}`);
-    console.log(".....................");
   } else {
     console.log("Error");
     console.log(`playerChoice = ${playerChoice}`);
@@ -99,16 +91,11 @@ function calcWinner(computerChoice, playerChoice) {
   return [computerWins, playerWins];
 }
 
-function playRound(playerChoice) {
-  computerChoice = getComputerChoice();
-  [computerWins, playerWins] = calcWinner();
-  return [computerWins, playerWins];
-}
-
 function playAgain() {
-  let again = prompt(
-    "Do you want to play again? Type 'yes' or 'no'"
-  ).toLowerCase();
+  playAgain.style.visibility = "visible";
+  //   let again = prompt(
+  //     "Do you want to play again? Type 'yes' or 'no'"
+  //   ).toLowerCase();
   while (again === "yes" && again === "no") {
     again = prompt(
       "Incorrect response. Do you want to play again? Type 'yes' or 'no'"
@@ -126,6 +113,6 @@ function playAgain() {
   return [again, rounds];
 }
 
-playRound();
+playRound(playerChoice);
 
 [again, rounds] = playAgain();
